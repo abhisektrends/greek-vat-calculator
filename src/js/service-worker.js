65628ace6232abb54fbdaef72cab2c102d7f9ec2
XhmikosR <xhmikosr@gmail.com>
2017-04-09 10:07:22 +0300

    Implement offline response.

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,1 +1,32 @@
-// empty for now
+(function serviceWorker () {
+    'use strict';
+
+    //var version = '0.1';
+
+    self.addEventListener('install', function (event) {
+        var timeStamp = Date.now();
+
+        event.waitUntil(caches.open('greek-vat-calc').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html?timestamp=' + timeStamp
+            ])
+            .then(function () {
+                return self.skipWaiting();
+            });
+        }));
+    });
+
+    self.addEventListener('activate', function (event) {
+        event.waitUntil(self.clients.claim());
+    });
+
+    self.addEventListener('fetch', function (event) {
+        event.respondWith(caches.match(event.request, {
+            ignoreSearch: true
+        })
+        .then(function (response) {
+            return response || fetch(event.request);
+        }));
+    });
+})();
